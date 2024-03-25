3b5de0683848e1d62d06dbd84000caa2b4161140
Ignacio19999 <ignaciomtangredi@hotmail.com>
2024-03-25 20:34:35 -0300

    Cambio 2

--- productManager.js
+++ productManager.js
@@ -1,19 +1,26 @@
-let productos = [
-    { nombre: 'PlayStation 5', precio: 499 },
-    { nombre: 'Xbox Series X', precio: 499 },
-    { nombre: 'Nintendo Switch', precio: 299 }
-];
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, 'products.json');
 
 function getProductos() {
-    return productos;
+    return JSON.parse(fs.readFileSync(productsFilePath, 'utf8'));
+}
+
+function guardarProductos(productos) {
+    fs.writeFileSync(productsFilePath, JSON.stringify(productos, null, 2), 'utf8');
 }
 
 function agregarProducto(producto) {
+    const productos = getProductos();
     productos.push(producto);
+    guardarProductos(productos);
 }
 
 function eliminarProducto(index) {
+    const productos = getProductos();
     productos.splice(index, 1);
+    guardarProductos(productos);
 }
 
 module.exports = { getProductos, agregarProducto, eliminarProducto };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const hbs = require('hbs');
 const path = require('path');
-const productManager = require('./productManager');
+const ProductManager = require('./ProductManager');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,19 +15,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('home');
 });
 
 app.get('/realtimeproducts', (req, res) => {
-    const productos = productManager.getProductos();
-    res.render('realtimeproducts', { productos });
+    const productos = ProductManager.getProductos();
+    res.render('realTimeProducts', { productos });
 });
 
 io.on('connection', (socket) => {
     console.log('Cliente conectado');
 
-    const productos = productManager.getProductos();
+    const productos = ProductManager.getProductos();
     socket.emit('productosActualizados', productos);
+
+    socket.on('agregarProducto', (producto) => {
+        ProductManager.agregarProducto(producto);
+        io.emit('productosActualizados', ProductManager.getProductos());
+    });
+
+    socket.on('eliminarProducto', (index) => {
+        ProductManager.eliminarProducto(index);
+        io.emit('productosActualizados', ProductManager.getProductos());
+    });
 });
 
 const PORT = process.env.PORT || 3000;
