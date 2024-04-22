58344d5468f2d3235f0ec078736c66b110717449
Ignacio19999 <ignaciomtangredi@hotmail.com>
2024-04-22 18:28:04 -0300

    cambios 1

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
-const consoleRoutes = require('./routes/consoles');
+const productRoutes = require('./routes/products');
 const cartRoutes = require('./routes/carts');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/tu_base_de_datos', { useNewUrlParser: true, useUnifiedTopology: true })
+const mongodbURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conexión a MongoDB establecida'))
   .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 app.use(express.json());
 
-app.use('/consoles', consoleRoutes);
+app.use('/products', productRoutes);
 app.use('/carts', cartRoutes);
 
 const PORT = process.env.PORT || 3000;
